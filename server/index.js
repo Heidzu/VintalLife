399c90786e805ea840dcb33c911e1d2d75ac44aa
Heidzu <frozen04040@gmail.com>
2026-04-25 11:20:10 +0300

    Remove Babel dependencies and convert JSX components to plain JS
    - Remove Babel dependencies from server/package.json
    - Delete server/babel.config.js
    - Remove @babel/register from server/index.js
    - Convert PhotoNewAction.jsx and PhotoUrlEditor.jsx to plain .js files
    - Delete obsolete .jsx component files

--- server/index.js
+++ server/index.js
@@ -1,14 +1,4 @@
-require('@babel/register')({
-  extensions: ['.js', '.jsx'],
-  only: [/[\\/]server[\\/]components[\\/]/],
-  presets: ['@babel/preset-env', '@babel/preset-react'],
-  plugins: [
-    '@babel/plugin-proposal-class-properties',
-    '@babel/plugin-proposal-object-rest-spread',
-    '@babel/plugin-syntax-import-assertions',
-    '@babel/plugin-transform-modules-commonjs'
-  ]
-});
+
 
 require('dotenv').config();
 
